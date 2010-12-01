bf66dddbe89e61455da58f6753ed47e2932de2a8
Devin Sevilla <devin@ning.com>
2010-11-30 17:53:42 -0800

    Default config now matches recent changes

--- config.py
+++ config.py
@@ -6,10 +6,12 @@ CONSUMER_KEY = "0d716e57-5ada-4b29-1459-2f4af1b26837"
 CONSUMER_SECRET = "f0963fa5-324-434f-86fc-8a17d14b16ca"
 NETWORK_SUBDOMAIN = "apiexample"
 
-NING_API_URL = "https://external.ningapis.com"
+NING_API_URL = "external.ningapis.com"
 
 def new_client(token=None):
     """Generate a new ningapi.Client instance"""
 
     consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
-    return ningapi.Client(NING_API_URL, NETWORK_SUBDOMAIN, consumer, token)
+    client = ningapi.Client(NING_API_URL, NETWORK_SUBDOMAIN, consumer, token)
+    client.method = oauth.SignatureMethod_PLAINTEXT()
+    return client
